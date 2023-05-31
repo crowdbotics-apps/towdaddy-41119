7474aab8e57e8effe0cacbbd7da400f1672351b2
Crowdbotics <team@crowdbotics.com>
2023-05-31 03:26:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/towdaddy_41119/MainActivity.java
@@ -0,0 +1,15 @@
+package com.towdaddy_41119;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "towdaddy_41119";
+  }
+}
